cb22d08e2bc1a9c5fed40622b9369368ed4694bd
p19ky <61684270+p19ky@users.noreply.github.com>
2020-04-26 13:59:18 +0300

    Added form of UnitTest-Film

---
+++ UnitTest/UnitTest.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include"../LAB5_OOP/Film.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest
+{
+	TEST_CLASS(UnitTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
